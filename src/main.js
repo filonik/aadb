6bc2b18948bcdc78276dca5c22bffe9c5912625c
filonik-cmu <filonik@users.noreply.github.com>
2024-05-11 20:51:39 -0400

    Added `Object.groupBy` polyfill

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './plugins/router'
 
+import groupBy from 'object.groupby'
+
+const shimmed = groupBy.shim()
+
 const app = createApp(App)
 
 app.use(router)
